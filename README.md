adab2b67d8b875907df3018e48a995393a2addec
Christopher Alexandre <christopher.alexandre.oliver@gmail.com>
2024-11-01 16:25:04 -0300

    :fire: initial project

---
+++ README.md
@@ -0,0 +1,3 @@
+# app_monitoria_dpd
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/app_monitoria_dpd/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_monitoria_dpd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
